8077121366b306f3b1af74436dc241409858b5f0
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-06-25 16:01:02 +1000

    Automate script execution

---
+++ utils/db/run_sql_scripts/run_sql_scripts.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+import os
+import psycopg
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
+
+db_host = os.getenv("DB_HOST")
+db_port = os.getenv("DB_PORT")
+db_name = os.getenv("DB_NAME")
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+
+
+def main(folder):
+    with psycopg.connect(
+        f"host={db_host} port={db_port} dbname={db_name} user={db_username} password={db_password}"
+    ) as conn:
+        with conn.cursor() as cur:
+            for sql_file in sorted(os.listdir(folder)):
+                if os.path.isfile(os.path.join(folder, sql_file)) and re.match(
+                    r"^[0-9]{2}", sql_file
+                ):
+                    print(sql_file)
+                    cur.execute(open(os.path.join(folder, sql_file), "r").read())
+            conn.commit()
+    return 0
+
+
+if __name__ == "__main__":
+    print("Scripts - Create Tables")
+    folder = "../setup/create_tables"
+    main(folder)
+    print("Scripts - Create Views")
+    folder = "../setup/create_views"
+    main(folder)
+    print("Scripts - Create Materialized Views")
+    folder = "../setup/create_materialized_views"
+    main(folder)
+    print("Scripts - Create Functions")
+    folder = "../functions"
+    main(folder)
+    print("Scripts - Create Triggers")
+    folder = "../triggers"
+    main(folder)
+    print("Scripts - Create Inserts")
+    folder = "../inserts"
+    main(folder)
