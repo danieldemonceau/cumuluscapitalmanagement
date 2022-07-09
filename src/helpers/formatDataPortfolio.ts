421a261afcf4c83dfcf6f3e25b36e55c6c29ac4f
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-07-09 23:52:40 +1000

    Date formatter

---
+++ src/helpers/formatDataPortfolio.ts
@@ -0,0 +1,20 @@
+import formatDate from "./formatDate";
+import { PositionOpen } from "../../types/Position.type";
+
+const formatDataPortfolio = (data: PositionOpen[]): PositionOpen[] => {
+  const dataFormatted = data.map((d) => {
+    return {
+      ...d,
+      openTimestamp: d.openTimestamp && formatDate(d.openTimestamp),
+      priceOpenAverage: +d.priceOpenAverage?.toFixed(2),
+      sharePriceCurrent:
+        d.sharePriceCurrent && +d.sharePriceCurrent?.toFixed(2),
+      sharePriceCurrentDate:
+        d.sharePriceCurrentDate && formatDate(d.sharePriceCurrentDate),
+      plPercent: +d.plPercent.toFixed(2),
+    };
+  });
+  return dataFormatted;
+};
+
+export default formatDataPortfolio;
