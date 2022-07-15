ebf1bf56437347467c843a5f021c277fa7b730b4
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-07-15 21:05:14 +1000

    Updates

--- utils/db/setup/create_views/01_position_open.sql
+++ utils/db/setup/create_views/01_position_open.sql
@@ -3,29 +3,32 @@ CREATE OR REPLACE VIEW position_open AS
 
 SELECT 
 p.id id
-, p.status "Position Status"
-, mt.direction "Position Type"
-, s.name "Asset"
-, p.open_timestamp open_timestamp
-, p.close_timestamp close_timestamp
+, p.status "status"
+, mt.direction "type"
+, s.name "asset"
+, p.open_timestamp "open_timestamp"
+, p.close_timestamp "close_timestamp"
 
-, pl.nb_of_units_opened nb_of_units_opened
-, pl.amount_opened amount_opened
-, pl.amount_opened / pl.nb_of_units_opened average_price_opened
+, pl.nb_of_units_opened "nb_of_units_opened"
+, pl.amount_opened "amount_opened"
+, pl.amount_opened / pl.nb_of_units_opened "price_opened_average"
 
-, pl.nb_of_units_closed nb_of_units_closed
-, pl.amount_closed amount_closed
-, pl.amount_closed / pl.nb_of_units_closed average_price_closed
+, pl.nb_of_units_closed "nb_of_units_closed"
+, pl.amount_closed "amount_closed"
+, pl.amount_closed / pl.nb_of_units_closed "price_closed_average"
 
-, pl.nb_of_units_open nb_of_units_open
-, pl.amount_open amount_open
-, pl.amount_open / pl.nb_of_units_open average_price_open
+, pl.nb_of_units_open "nb_of_units_open"
+, pl.amount_open "amount_open"
+, (pl.nb_of_units_open * ql.qcurrent_price) "amount_open_current"
+, pl.amount_open / pl.nb_of_units_open "price_open_average"
 
-, EXTRACT(DAY FROM COALESCE(close_timestamp
-, CURRENT_TIMESTAMP(0)) - open_timestamp) holding_period_days
-, (pl.amount_opened - pl.amount_closed) pl
-, (pl.amount_opened - pl.amount_closed) / pl.amount_opened * 100. pl_percent
-, st.name strategy_name
+, ql.qcurrent_price "share_price_current"
+, ql.qtimestamp "share_price_current_date"
+
+, EXTRACT(DAY FROM COALESCE(close_timestamp, CURRENT_TIMESTAMP(0)) - open_timestamp) "holding_period_days"
+, COALESCE(pl.amount_closed, 0::MONEY) + (pl.nb_of_units_open * ql.qcurrent_price - pl.amount_open) "pl_absolute"
+, ((COALESCE(pl.amount_closed, 0::MONEY) + (pl.nb_of_units_open * ql.qcurrent_price - pl.amount_open)) / pl.amount_opened) * 100 pl_percent
+, st.name "strategy_name"
 FROM position p
 JOIN position_market_transaction pmt ON pmt.position_id = p.id
 JOIN market_transaction mt ON mt.id = pmt.market_transaction_id
@@ -113,6 +116,15 @@ JOIN (
     ) mtt ON (mtt.pid = mtc.pid) OR (mtt.pid = mto.pid)
 ) pl ON pl.pid = p.id
 JOIN strategy st ON st.id = mt.strategy_id
+LEFT JOIN (
+    SELECT q.symbol_id qsymbol_id, q.close qcurrent_price, q.timestamp qtimestamp
+    FROM quote q
+    JOIN (
+        SELECT q.symbol_id qsymbol_id, MAX(q.timestamp) qtimestamp
+        FROM quote q
+        GROUP BY q.symbol_id
+    ) ql ON ql.qsymbol_id = q.symbol_id AND q.timestamp = ql.qtimestamp
+) ql ON ql.qsymbol_id = s.id AND ql.qtimestamp > p.open_timestamp
 WHERE p.status = 'Open'
 GROUP BY 
 p.id
@@ -127,6 +139,8 @@ p.id
 , pl.amount_closed
 , pl.nb_of_units_open
 , pl.amount_open
+, ql.qcurrent_price
+, ql.qtimestamp
 , st.name
 ;
 
