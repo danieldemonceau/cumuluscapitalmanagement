a8378ca3c9390ec7eb0b2378446cf22ff55562c1
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-07-20 23:25:29 +1000

    Add trigger

---
+++ utils/db/triggers/03_quote_insert_after.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE FUNCTION trigger_quote_insert_after() RETURNS TRIGGER AS $$
+    DECLARE
+        v_quote_close DECIMAL = NEW.close;
+        v_quote_symbol_id INTEGER = NEW.symbol_id;
+    BEGIN
+        RAISE NOTICE '- - - - - - - - - - - - - - - - - - - - - ';
+        RAISE NOTICE 'TRIGGER trigger_quote_insert_after - START';
+        RAISE NOTICE '- - - - - - - - - - - - - - - - - - - - - ';
+
+        UPDATE position p
+        SET pl_percent = (v_quote_close - po.average_price_open) / po.average_price_open
+        FROM position_open po
+        JOIN position_market_transaction pmt ON pmt.position_id = po.id
+        JOIN market_transaction mt ON mt.id = pmt.market_transaction_id
+        JOIN symbol s ON s.id = mt.symbol_id
+        WHERE 1 = 1
+        AND po.id = p.id
+        AND p.id = pmt.position_id
+        AND s.id = v_quote_symbol_id;
+
+        RAISE NOTICE '- - - - - - - - - - - - - - - - - - - - - ';
+        RAISE NOTICE 'TRIGGER trigger_quote_insert_after - END';
+        RAISE NOTICE '- - - - - - - - - - - - - - - - - - - - - ';
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_quote_insert_after
+AFTER INSERT
+ON quote
+FOR EACH ROW EXECUTE FUNCTION trigger_quote_insert_after();
