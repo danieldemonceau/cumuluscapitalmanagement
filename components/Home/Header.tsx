4ba58d95a78c02b95618dd71828a9d3e826f26f0
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-08-11 23:31:20 +1000

    New hero

--- components/Home/Header.tsx
+++ components/Home/Header.tsx
@@ -1,11 +1,17 @@
-import heroImage from "./hero.png";
+import heroImage from "./hero.jpg";
 import Image from "next/image";
 
 const Header = () => {
   return (
     <>
       <header className="bg-black text-gray-300">
-        <Image className="z-0" src={heroImage} alt="Hero Image" />
+        <Image
+          className="h-4/5 object-cover object-top"
+          src={heroImage}
+          alt="Hero Image"
+          layout="responsive"
+          height={`2100px`}
+        />
       </header>
     </>
   );
