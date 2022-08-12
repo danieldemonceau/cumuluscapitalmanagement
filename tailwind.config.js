8fbd177214552f986e81b3ea06c0e24ef56a38bc
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-08-12 23:22:25 +1000

    Import Fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        cormorantGaramond: ["Cormorant Garamond", "serif"],
+        oranienbaum: ["Oranienbaum", "serif"],
+        imbue: ["Imbue", "serif"],
+        frenchCanon: ["IM Fell French Canon SC", "serif"],
+        lato: ["Lato", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
