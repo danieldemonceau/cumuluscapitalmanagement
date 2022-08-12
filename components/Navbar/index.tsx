c29addf6d74a2bd047536e3a4570df7918390c08
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-08-12 23:22:16 +1000

    Add Clouds icon

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,11 +1,18 @@
 import Link from "next/link";
 import HorizontalBar from "./HorizontalBar";
+import Image from "next/image";
+import CloudImage from "./clouds.png";
 
 const Navbar = () => {
   return (
     <>
-      <nav className="z-50 py-6 left-0 right-0 m-auto my-auto scroll-smooth">
+      <nav className="z-50 py-6 left-0 right-0 m-auto my-auto">
         <div className="flex justify-center items-center">
+          <div className="absolute left-0 p-3 ml-6">
+            <a href="https://fluffyclouds.com.au">
+              <Image src={CloudImage} alt={`Cloud Icon`}></Image>
+            </a>
+          </div>
           <nav className="text-black h-auto">
             <ul className="flex flex-row space-x-12">
               <li className="uppercase hover:text-green-50">
