dde614b8f50c354c9a6a45257faae2cd97fec7be
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-08-14 17:47:57 +1000

    Reorganize project structure

---
+++ components/Layout/index.tsx
@@ -0,0 +1,23 @@
+import Head from "next/head";
+import Footer from "./Footer";
+import Navbar from "./Navbar";
+import { NextSeo } from "next-seo";
+import meta from "../../src/data/meta";
+import { useEffect, useState } from "react";
+import ContactUs from "./ContactUs";
+
+const Layout = ({ children, title }: any) => {
+  return (
+    <>
+      {/* <Head></Head> */}
+      {/* SEO config */}
+      {/* <NextSeo title="Home" description={`Homepage of ${meta.domainName}`} /> */}
+      <Navbar />
+      <main>{children}</main>
+      <ContactUs />
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
