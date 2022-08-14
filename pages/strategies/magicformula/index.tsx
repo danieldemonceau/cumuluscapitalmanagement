10c281310a3ac62faa5ffdf8227fc8cffbc59548
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-08-14 17:46:29 +1000

    Magic Formula page

---
+++ pages/strategies/magicformula/index.tsx
@@ -0,0 +1,14 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import MagicFormula from "../../../components/Strategies/MagicFormula";
+
+const Index: NextPage = () => {
+  return (
+    <>
+      <MagicFormula />
+    </>
+  );
+};
+
+export default Index;
