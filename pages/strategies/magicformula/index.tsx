7e754822b55360c382a13d36c060c0e02719e6e5
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-09-10 19:47:54 +1000

    Magic Formula page + images

--- pages/strategies/magicformula/index.tsx
+++ pages/strategies/magicformula/index.tsx
@@ -1,14 +1,40 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import MagicFormula from "../../../components/Strategies/MagicFormula";
+import axios from "axios";
+import type { InferGetServerSidePropsType } from "next";
+import { POSITION_OPEN_MAGIC_FORMULAS } from "@/components/ApolloClient/allPositionOpenMagicFormulas";
+// import Head from "next/head";
+// import styles from "../styles/Home.module.css";
+import MagicFormula from "@/components/Strategies/MagicFormula";
+import meta, { httpProtocol } from "@/src/data/meta";
+import formatDataPortfolio from "@/src/helpers/formatDataPortfolio";
+import { PositionOpen } from "@/src/types/Position.type";
 
-const Index: NextPage = () => {
+const Index = ({
+  positionOpen,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   return (
     <>
-      <MagicFormula />
+      <MagicFormula positionOpen={positionOpen} />
     </>
   );
+};
+
+export const getServerSideProps = async () => {
+  // Get last Quotes
+  const response = await axios({
+    baseURL: `${httpProtocol}://${meta.domainName}:${process.env.PORT}`,
+    url: `/api/graphql`,
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+    },
+    data: { query: POSITION_OPEN_MAGIC_FORMULAS },
+  });
+
+  const positionOpen: PositionOpen[] = formatDataPortfolio(
+    response.data.data.allPositionOpenMagicFormulas.nodes
+  );
+
+  return { props: { positionOpen } };
 };
 
 export default Index;
