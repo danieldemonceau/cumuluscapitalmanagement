b4bff48021e0966bb340a2a3f988a492c88ed4ec
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-09-24 16:05:24 +1000

    Bug fix

--- src/helpers/formatDate.ts
+++ src/helpers/formatDate.ts
@@ -1,5 +1,6 @@
 const formatDate = (date: string) => {
-  const [dateStr] = new Date(date).toISOString().split("T");
+  // const [dateStr] = new Date(date).toISOString().split("T");
+  const [dateStr] = date.split("T");
   return dateStr;
 };
 
