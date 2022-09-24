5301825bd4e1957fc175b844727f94f724756d64
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-09-24 16:05:10 +1000

    Remove credit to put on specific page

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -7,21 +7,6 @@ const Footer = () => {
       <footer className="">
         <div className="text-white text-lg bg-slate-900">
           <AcknowledgementOfCountry />
-          {/* Credits */}
-          <div className="text-sm">
-            <div>Credits</div>
-            <a
-              target="_blank"
-              href="https://icons8.com/icon/OFU5h8HeWK3z/clouds"
-              rel="noreferrer"
-            >
-              Clouds
-            </a>{" "}
-            icon by{" "}
-            <a target="_blank" href="https://icons8.com" rel="noreferrer">
-              Icons8
-            </a>
-          </div>
           <Ribbon />
         </div>
       </footer>
