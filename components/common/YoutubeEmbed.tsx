acd9c5b3c8a66cc87c3704865f9c85311d8fbedd
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-09-24 13:42:21 +1000

    Add Youtube iframe

--- components/common/YoutubeEmbed.tsx
+++ components/common/YoutubeEmbed.tsx
@@ -4,8 +4,8 @@ import PropTypes from "prop-types";
 const YoutubeEmbed = ({ embedId }: { embedId: string }) => (
   <div className="m-5 aspect-h-3">
     <iframe
-      width="560"
-      height="315"
+      width="940"
+      height="473"
       src={`https://www.youtube.com/embed/${embedId}`}
       frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
