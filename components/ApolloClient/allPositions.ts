78a03dac622005ae8471062d7e41e9aefa499e21
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-09-25 18:09:10 +1000

    Rename variable

--- components/ApolloClient/allPositions.ts
+++ components/ApolloClient/allPositions.ts
@@ -9,8 +9,8 @@ export const ALL_POSITIONS_OPEN_MAGIC_FORMULA = `
         asset
         openTimestamp
         priceOpenedAverage
-        sharePriceCurrent
-        sharePriceCurrentDate
+        priceCurrent
+        priceCurrentDate
         plPercent
         strategyName
       }
@@ -45,8 +45,8 @@ export const ALL_POSITIONS_OPEN_THE_ACQUIRERS_MULTIPLE = `
         asset
         openTimestamp
         priceOpenedAverage
-        sharePriceCurrent
-        sharePriceCurrentDate
+        priceCurrent
+        priceCurrentDate
         plPercent
         strategyName
       }
@@ -57,6 +57,42 @@ export const ALL_POSITIONS_OPEN_THE_ACQUIRERS_MULTIPLE = `
 export const ALL_POSITIONS_CLOSED_THE_ACQUIRERS_MULTIPLE = `
   query GetAllPositionClosedTheAcquirersMultiples {
     allPositionClosedTheAcquirersMultiples {
+      nodes {
+        id
+        type
+        asset
+        openTimestamp
+        priceOpenedAverage
+        closeTimestamp
+        priceClosedAverage
+        plPercent
+        strategyName
+      }
+    }
+  }
+`;
+
+export const ALL_POSITIONS_OPEN_MARKET_DOLLAR_COST_AVERAGING = `
+  query GetAllPositionOpenMarketDollarCostAveragings {
+    allPositionOpenMarketDollarCostAveragings {
+      nodes {
+        id
+        type
+        asset
+        openTimestamp
+        priceOpenedAverage
+        priceCurrent
+        priceCurrentDate
+        plPercent
+        strategyName
+      }
+    }
+  }
+`;
+
+export const ALL_POSITIONS_CLOSED_MARKET_DOLLAR_COST_AVERAGING = `
+  query GetAllPositionClosedMarketDollarCostAveragings {
+    allPositionClosedMarketDollarCostAveragings {
       nodes {
         id
         type
