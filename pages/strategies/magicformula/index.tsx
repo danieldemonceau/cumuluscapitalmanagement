fb53965a3444957212d5d0af4f9878b9d7f0ffb0
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-12-28 18:24:20 +0100

    Load from cache if available

--- pages/strategies/magicformula/index.tsx
+++ pages/strategies/magicformula/index.tsx
@@ -12,6 +12,10 @@ import {
   formatDataPortfolioClosedPositions,
 } from "@/src/helpers/formatDataPortfolio";
 import { PositionOpen, PositionClosed } from "@/src/types/Position.type";
+import * as fs from "fs";
+import * as path from "path";
+import { loadJsonFile } from "load-json-file";
+import { writeJsonFile } from "write-json-file";
 
 const Index = ({
   positionOpen,
@@ -29,14 +33,50 @@ const Index = ({
 
 export const getServerSideProps = async () => {
   // Get last Quotes
+  // Check if the data is in local file
+  const todayYYMMDD = new Date()
+    .toLocaleDateString("en-AU")
+    .split("/")
+    .reverse()
+    .join("")
+    .split("")
+    .splice(2)
+    .join("");
+
+  let readAPI = true;
+
+  const fileNamePositionsOpen = `mf-positions-open-${todayYYMMDD}.json`;
+  const fileNamePositionsClosed = `mf-positions-closed-${todayYYMMDD}.json`;
+
+  const files = await fs.promises.readdir("./cache");
+  if (files.filter((fn) => fn === fileNamePositionsOpen).length > 0)
+    readAPI = false;
+
+  if (!readAPI) {
+    const positionOpen: PositionOpen[] = await loadJsonFile(
+      path.join(process.cwd(), "cache", fileNamePositionsOpen)
+    );
+    const positionClosed: PositionClosed[] = await loadJsonFile(
+      path.join(process.cwd(), "cache", fileNamePositionsClosed)
+    );
+
+    // Delete old cache files
+    const filesToDelete = files.filter(
+      (fn) =>
+        (fn.includes("mf-positions-open") ||
+          fn.includes("mf-positions-closed")) &&
+        fn !== fileNamePositionsOpen &&
+        fn !== fileNamePositionsClosed
+    );
+    for (const file of filesToDelete) {
+      await fs.promises.unlink(path.join(process.cwd(), "cache", file));
+    }
+
+    return { props: { positionOpen, positionClosed } };
+  }
+
+  // If not, get the data from the API
   const responsePositionOpen = await axios({
-    // baseURL: `${process.env.GRAPHQL_PROTOCOL}://${
-    //   process.env.GRAPHQL_DOMAIN_NAME
-    // }${
-    //   process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
-    //     ? ""
-    //     : ":" + process.env.GRAPHQL_PORT
-    // }`,
     url: `${process.env.GRAPHQL_PROTOCOL}://${process.env.GRAPHQL_DOMAIN_NAME}${
       process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
         ? ""
@@ -54,13 +94,6 @@ export const getServerSideProps = async () => {
   );
 
   const responsePositionClosed = await axios({
-    // baseURL: `${process.env.GRAPHQL_PROTOCOL}://${
-    //   process.env.GRAPHQL_DOMAIN_NAME
-    // }${
-    //   process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
-    //     ? ""
-    //     : ":" + process.env.GRAPHQL_PORT
-    // }`,
     url: `${process.env.GRAPHQL_PROTOCOL}://${process.env.GRAPHQL_DOMAIN_NAME}${
       process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
         ? ""
@@ -75,6 +108,12 @@ export const getServerSideProps = async () => {
 
   const positionClosed: PositionClosed[] = formatDataPortfolioClosedPositions(
     responsePositionClosed.data.data.allPositionClosedMagicFormulas.nodes
+  );
+
+  await writeJsonFile(path.join("cache", fileNamePositionsOpen), positionOpen);
+  await writeJsonFile(
+    path.join("cache", fileNamePositionsClosed),
+    positionClosed
   );
 
   return { props: { positionOpen, positionClosed } };
--- pages/strategies/theacquirersmultiple/index.tsx
+++ pages/strategies/theacquirersmultiple/index.tsx
@@ -12,6 +12,10 @@ import {
   formatDataPortfolioClosedPositions,
 } from "@/src/helpers/formatDataPortfolio";
 import { PositionOpen, PositionClosed } from "@/src/types/Position.type";
+import * as fs from "fs";
+import * as path from "path";
+import { loadJsonFile } from "load-json-file";
+import { writeJsonFile } from "write-json-file";
 
 const Index = ({
   positionOpen,
@@ -28,14 +32,51 @@ const Index = ({
 };
 
 export const getServerSideProps = async () => {
+  // Get last Quotes
+  // Check if the data is in local file
+  const todayYYMMDD = new Date()
+    .toLocaleDateString("en-AU")
+    .split("/")
+    .reverse()
+    .join("")
+    .split("")
+    .splice(2)
+    .join("");
+
+  let readAPI = true;
+
+  const fileNamePositionsOpen = `am-positions-open-${todayYYMMDD}.json`;
+  const fileNamePositionsClosed = `am-positions-closed-${todayYYMMDD}.json`;
+
+  const files = await fs.promises.readdir("./cache");
+  if (files.filter((fn) => fn === fileNamePositionsOpen).length > 0)
+    readAPI = false;
+
+  if (!readAPI) {
+    const positionOpen: PositionOpen[] = await loadJsonFile(
+      path.join(process.cwd(), `./cache/${fileNamePositionsOpen}`)
+    );
+    const positionClosed: PositionClosed[] = await loadJsonFile(
+      path.join(process.cwd(), `./cache/${fileNamePositionsClosed}`)
+    );
+
+    // Delete old cache files
+    const filesToDelete = files.filter(
+      (fn) =>
+        (fn.includes("am-positions-open") ||
+          fn.includes("am-positions-closed")) &&
+        fn !== fileNamePositionsOpen &&
+        fn !== fileNamePositionsClosed
+    );
+    for (const file of filesToDelete) {
+      await fs.promises.unlink(path.join(process.cwd(), "cache", file));
+    }
+
+    return { props: { positionOpen, positionClosed } };
+  }
+
+  // If not, get the data from the API
   const responsePositionOpen = await axios({
-    // baseURL: `${process.env.GRAPHQL_PROTOCOL}://${
-    //   process.env.GRAPHQL_DOMAIN_NAME
-    // }${
-    //   process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
-    //     ? ""
-    //     : ":" + process.env.GRAPHQL_PORT
-    // }`,
     url: `${process.env.GRAPHQL_PROTOCOL}://${process.env.GRAPHQL_DOMAIN_NAME}${
       process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
         ? ""
@@ -53,13 +94,6 @@ export const getServerSideProps = async () => {
   );
 
   const responsePositionClosed = await axios({
-    // baseURL: `${process.env.GRAPHQL_PROTOCOL}://${
-    //   process.env.GRAPHQL_DOMAIN_NAME
-    // }${
-    //   process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
-    //     ? ""
-    //     : ":" + process.env.GRAPHQL_PORT
-    // }`,
     url: `${process.env.GRAPHQL_PROTOCOL}://${process.env.GRAPHQL_DOMAIN_NAME}${
       process.env.GRAPHQL_PORT && +process.env.GRAPHQL_PORT === 80
         ? ""
@@ -75,6 +109,12 @@ export const getServerSideProps = async () => {
   const positionClosed: PositionClosed[] = formatDataPortfolioClosedPositions(
     responsePositionClosed.data.data.allPositionClosedTheAcquirersMultiples
       .nodes
+  );
+
+  await writeJsonFile(path.join("cache", fileNamePositionsOpen), positionOpen);
+  await writeJsonFile(
+    path.join("cache", fileNamePositionsClosed),
+    positionClosed
   );
 
   return { props: { positionOpen, positionClosed } };
