bce98d4e56924b84e858840e21f0056d481de43c
Daniel Demonceau <daniel.demonceau@yahoo.com>
2022-12-29 16:25:22 +0100

    Youtube iframe formatting

--- components/common/YoutubeEmbed.tsx
+++ components/common/YoutubeEmbed.tsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
 const YoutubeEmbed = ({ embedId }: { embedId: string }) => (
-  <div className="m-5 aspect-h-3">
+  <div className="iframe-container">
     <iframe
-      width="940"
-      height="473"
+      // width="940"
+      // height="473"
+      // className="w-full aspect-video hover:aspect-square"
+      className="responsive-iframe hover:aspect-square"
       src={`https://www.youtube.com/embed/${embedId}`}
-      frameBorder="0"
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
       title="Embedded youtube"
