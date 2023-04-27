cfd116206a3942aa1681ae9331c0e1bd090aba3f
Daniel Demonceau <daniel.demonceau@yahoo.com>
2023-04-27 22:20:16 +1000

    Add trpc

---
+++ src/pages/_app.tsx
@@ -0,0 +1,38 @@
+import "../../styles/globals.css";
+import type { AppProps } from "next/app";
+
+import Layout from "@/components/Layout";
+import { NextSeo } from "next-seo";
+import SEO from "../../next-seo.config";
+import Meta from "@/components/Meta";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "@/components/ApolloClient";
+
+import type { AppType } from "next/app";
+import { trpc } from "../utils/trpc";
+
+// import { Provider } from "react-redux";
+// import { store } from "@/src//state/store";
+
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+config.autoAddCss = false;
+
+const App: AppType = ({ Component, pageProps }: AppProps) => {
+  return (
+    <>
+      {/* <Provider store={store}> */}
+      <NextSeo {...SEO} />
+      <Meta />
+      <ApolloProvider client={apolloClient}>
+        {/* <Layout title={pageProps.title} className="h-full"> */}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+      {/* </Provider> */}
+    </>
+  );
+};
+
+export default trpc.withTRPC(App);
