5fc6f04c974ac7ff60e902b326a4049043c1b771
Daniel Demonceau <daniel.demonceau@yahoo.com>
2023-05-01 20:57:20 +1000

    Remove graphql

--- src/data/meta.tsx
+++ src/data/meta.tsx
@@ -2,14 +2,12 @@ const author = "Daniel Demonceau, Fluffy Clouds Avenue Pty Ltd";
 const authorCompanyUrl = "FluffyCloudsAvenue.com";
 const authorWebsiteUrl = "daniel.fashion";
 const domainName = "cumuluscapitalmanagement.com";
-const graphqlDomainName = "localhost";
 const websiteNameShort = "Cumulus";
 const websiteNameLong = "Cumulus Capital Management";
 export const httpProtocol = "http";
 const meta = {
   author,
   domainName,
-  graphqlDomainName,
   websiteNameShort,
   websiteNameLong,
   keywords: `${author}, ${authorCompanyUrl}, ${authorWebsiteUrl}, Investment, Investing, Capital, Wealth, Funds, Melbourne, Australia, Magic Fomula, Acquirer's Multiple, ${domainName}`,
